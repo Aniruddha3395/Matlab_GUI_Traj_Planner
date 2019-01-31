557ef7413cb8a696e1863f170d51144d25f3e207
aniruddha3395 <ani.shembekar@gmail.com>
2019-01-31 12:16:31 -0800

    cleanse

--- Viz_tools/CloseFigWindow.m
+++
@@ -1,5 +0,0 @@
-function CloseFigWindow(~,~)
-
-close all;
-
-end
--- iiwa/iiwa_manipulability_from_joints.m
+++
@@ -1,9 +0,0 @@
-function M = iiwa_manipulability_from_joints(thetas)
-    M = [];
-    for i = 1:size(thetas,1)
-        J = iiwa_analytical_jacobian(thetas(i,:));
-        s = svd(J);
-        M = [M,prod(s)];
-    end
-end
-
